3afce658f2611cd7b30a12cd02f6c51fdbfec10b
dev-dede <tsewayog@gmail.com>
2024-12-28 14:26:18 +0000

    Add initial implementation of Mad Lib story generator

---
+++ 0-control_flow/0-mad_libs.py
@@ -0,0 +1,13 @@
+day_adjective = input("Enter an adjective to describe the day (eg.. windy, bright): ")
+monkey_adjective = input("Enter an adjective to describe a funny monkey (eg., cute, silly): ")
+lion_adjective = input("Enter an adjective to describe a lion (eg., large, ferocious): ")
+experience_adjective = input("Enter an adjective to describe your experience for the day(eg. wonderful, frightening): ")
+#ending = ""
+
+story = f"""
+On a beautiful {day_adjective} day, I went to the zoo. 
+I saw a funny {monkey_adjective} monkey swinging from the trees. 
+Then, I spotted a majestic {lion_adjective} lion lounging in the sun.  
+What a wild and {experience_adjective} experience!
+"""
+print(story)
---
+++ 0-control_flow/README.md
@@ -0,0 +1,2 @@
+Get creative and have some fun with Python! 
+This challenges will help you solidify your understanding of conditional statements while putting your problem-solving skills to the test.
