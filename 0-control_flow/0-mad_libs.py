174e5a834cb653887f41502a1014eb5809650a2c
dev-dede <tsewayog@gmail.com>
2024-12-28 16:07:54 +0000

    Refactor code to make code cleaner and enhance clarity

--- 0-control_flow/0-mad_libs.py
+++ 0-control_flow/0-mad_libs.py
@@ -1,35 +1,22 @@
 day_adjective = input("Enter an adjective to describe the day (eg.. windy, bright): ")
 while day_adjective == "beautiful":
-    print("Please enter any adjective apart from beautiful")
-    day_adjective = input("Enter an adjective to describe the day (eg.. windy, bright): ")
+    day_adjective = input("Please enter any adjective apart from beautiful: ")
 
 monkey_adjective = input("Enter an adjective to describe a funny monkey (eg., cute, silly): ")
-while monkey_adjective == "funny":
-    print("Please enter any adjective other than funny")
-    monkey_adjective = input("Enter an adjective to describe a funny monkey (eg., cute, silly): ")
+while monkey_adjective == "funny": #Validate input to prevent repetition since adjective funny would be used in story
+    monkey_adjective = input("Please enter any adjective other than funny: ")
 
 lion_adjective = input("Enter an adjective to describe a lion (eg., large, ferocious): ")
-while lion_adjective == "majestic":
-    print("Please enter any adjective other than majestic")
-    lion_adjective = input("Enter an adjective to describe a lion (eg., large, ferocious): ")
+while lion_adjective == "majestic": #Validate input to prevent repetition since adjective majestic would be used in story
+    lion_adjective = input("Please enter any adjective other than majestic: ")
 
 experience_adjective = input("Enter an adjective to describe your experience for the day(eg. wonderful, frightening): ")
-while experience_adjective == "wild":
-     print("Please enter any adjective other than wild")
-     experience_adjective = input("Enter an adjective to describe your experience for the day(eg. wonderful, frightening): ")
+while experience_adjective == "wild": #Validate input to prevent repetition since adjective wild would be used in story
+     experience_adjective = input("Please enter any adjective other than wild: ")
+ 
+feelings_for_monkeys = "I love monkeys" if monkey_adjective not in ["ugly", "annoying"] else  "I am not a fun of monkeys"
 
-ending = ""
-feelings_for_monkeys = ""
-
-if monkey_adjective == "ugly" or monkey_adjective == "annoying":
-    feelings_for_monkeys = "I am not a fun of monekys"
-else:
-    feelings_for_monkeys =  "I love monkeys!!"
-
-if experience_adjective == "frightening" or experience_adjective == "boring":
-    ending = "I am never going to the zoo again"
-else:
-    ending = "I really loved the zoo"
+ending = "I really loved the zoo" if experience_adjective not in ["frightening", "boring"] else "I am never going to the zoo again"
 
 story = f"""
 On a beautiful {day_adjective} day, I went to the zoo. 
