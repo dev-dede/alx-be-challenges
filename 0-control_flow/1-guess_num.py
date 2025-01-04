c37bc7816dfb5efc6dbe631f188f9f6d1aacad3c
dev-dede <tsewayog@gmail.com>
2025-01-04 19:20:26 +0000

    Add loop to enable user to pay the game multiple times till they decide to quit

--- 0-control_flow/1-guess_num.py
+++ 0-control_flow/1-guess_num.py
@@ -1,10 +1,23 @@
 import random
-secret_number = random.randint(1, 10)
-guess = int(input("I'm thinking of a number between 1 and 10. Can you guess it? "))
-while guess != secret_number:
-    match guess:
-        case _ if guess < secret_number:
-            guess = int(input("Nope, your guess is a bit low. Give it another shot! "))
-        case _ if guess > secret_number:
-            guess = int(input("Nope, your guess is a bit high. Give it another shot! "))
-print("Congratulations, you guessed it!")
+
+#Main game loop
+while True:
+    secret_number = random.randint(1, 10)
+    guess = int(input("I'm thinking of a number between 1 and 10. Can you guess it? "))
+
+    #Loop until user gets the guess correct
+    while guess != secret_number:
+        match guess:
+            case _ if guess < secret_number:
+                guess = int(input("Nope, your guess is a bit low. Give it another shot! "))
+            case _ if guess > secret_number:
+                guess = int(input("Nope, your guess is a bit high. Give it another shot! "))
+    print("Congratulations, you guessed it!")
+
+    #Ask player if they want to play again
+    play_again = input("Play again? (yes/no) ").lower()
+    if play_again == "yes":
+        print("....New game starts!")
+    else:
+        print("Thanks for playing! Goodbye!")
+        break
