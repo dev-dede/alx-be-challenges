64449fd611fdb331cd80215646b4bef8271237db
dev-dede <tsewayog@gmail.com>
2025-01-04 19:33:50 +0000

    Enhance code to print number of guesses it took for the player to win the game

--- 0-control_flow/1-guess_num.py
+++ 0-control_flow/1-guess_num.py
@@ -4,6 +4,7 @@ import random
 while True:
     secret_number = random.randint(1, 10)
     guess = int(input("I'm thinking of a number between 1 and 10. Can you guess it? "))
+    number_guesses = 1
 
     #Loop until user gets the guess correct
     while guess != secret_number:
@@ -11,8 +12,10 @@ while True:
             case _ if guess < secret_number:
                 guess = int(input("Nope, your guess is a bit low. Give it another shot! "))
             case _ if guess > secret_number:
-                guess = int(input("Nope, your guess is a bit high. Give it another shot! "))
+                guess = int(input("Oops, your guess is a bit high. Try again! "))
+        number_guesses += 1
     print("Congratulations, you guessed it!")
+    print(f"You got it right after {number_guesses} guesses")
 
     #Ask player if they want to play again
     play_again = input("Play again? (yes/no) ").lower()
