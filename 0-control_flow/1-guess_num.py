dac30b4baaff90ef9faa7969794387fc6a36b9e0
dev-dede <tsewayog@gmail.com>
2025-01-04 18:46:05 +0000

    Enhance Guess Secret Number game by adding cases of guess being less or greater than secret number

--- 0-control_flow/1-guess_num.py
+++ 0-control_flow/1-guess_num.py
@@ -5,6 +5,8 @@ guess = int(input("I'm thinking of a number between 1 and 10. Can you guess it? 
 match guess:
     case _ if guess == secret_number:
         print("Congratulations, you guessed it!")
-    case _:
-        print("Nope")
+    case _ if guess < secret_number:
+        print("Nope, your guess is a bit low. Give it another shot!")
+    case _ if guess > secret_number:
+        print("Nope, your guess is a bit high. Give it another shot!")
 print(secret_number)
