34777fb2ba5ffb2783501bfc0649d5afc98c4690
dev-dede <tsewayog@gmail.com>
2025-01-04 18:58:46 +0000

    Add while loop for user to continue guessing tell they get the right number

--- 0-control_flow/1-guess_num.py
+++ 0-control_flow/1-guess_num.py
@@ -1,12 +1,10 @@
 import random
 secret_number = random.randint(1, 10)
 guess = int(input("I'm thinking of a number between 1 and 10. Can you guess it? "))
-
-match guess:
-    case _ if guess == secret_number:
-        print("Congratulations, you guessed it!")
-    case _ if guess < secret_number:
-        print("Nope, your guess is a bit low. Give it another shot!")
-    case _ if guess > secret_number:
-        print("Nope, your guess is a bit high. Give it another shot!")
-print(secret_number)
+while guess != secret_number:
+    match guess:
+        case _ if guess < secret_number:
+            guess = int(input("Nope, your guess is a bit low. Give it another shot! "))
+        case _ if guess > secret_number:
+            guess = int(input("Nope, your guess is a bit high. Give it another shot! "))
+print("Congratulations, you guessed it!")
